42e80d6d498c09e0b5e1150f73561a6f3ea678e6
Alancittoo <echeniquealan@yahoo.com>
2023-05-17 17:44:50 -0400

    pin component started

---
+++ react-app/src/store/pin.js
@@ -0,0 +1,5 @@
+export const GET_PINS = 'pins/GET_PINS'
+export const GET_SINGLE_PIN = 'pins/GET_SINGLE_PIN'
+export const UPDATE_PIN = 'pins/UPDATE_PIN'
+export const DELETE_PIN = 'pins/DELETE_PIN'
+
