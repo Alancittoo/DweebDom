51e502ef123edadd4d845043a671d2d6080c9f7e
Alancittoo <echeniquealan@yahoo.com>
2023-05-17 11:46:54 -0400

    testing

--- app/models/user.py
+++ app/models/user.py
@@ -25,13 +25,13 @@ class User(db.Model, UserMixin):
     user_likes_relationship = db.relationship('Like', back_populates='like_users_relationship') #done
 
     # Asscociation
-    followed = db.relationship(
-        'User', secondary='follows', primaryjoin=(follows.c.follower_id == id), secondaryjoin=(follows.c.followed_id == id), foreign_keys=[follows.c.follower_id, follows.c.followed_id], back_populates='followers'
-    )
+    # followed = db.relationship(
+    #     'User', secondary='follows', primaryjoin=(follows.c.follower_id == id), secondaryjoin=(follows.c.followed_id == id), foreign_keys=[follows.c.follower_id, follows.c.followed_id], back_populates='followers'
+    # )
 
-    followers = db.relationship(
-        'User', secondary='follows', primaryjoin=(follows.c.followed_id == id), secondaryjoin=(follows.c.follower_id == id), foreign_keys=[follows.c.followed_id, follows.c.follower_id], back_populates='followed'
-    )
+    # followers = db.relationship(
+    #     'User', secondary='follows', primaryjoin=(follows.c.followed_id == id), secondaryjoin=(follows.c.follower_id == id), foreign_keys=[follows.c.followed_id, follows.c.follower_id], back_populates='followed'
+    # )
 
     @property
     def password(self):
